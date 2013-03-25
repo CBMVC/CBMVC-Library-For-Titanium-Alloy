e1e606342858bf3c888c89e88a4eee2d7def5783
winsonet <winsonet@gmail.com>
2013-03-25 22:35:25 +0800

    update static sample code
    update static sample code

--- app/controllers/agenda.js
+++ app/controllers/agenda.js
@@ -3,6 +3,14 @@ exports.baseController = "base";
 $.onLoad = function() {
     var data = $.getData();
     Alloy.Globals.CB.Debug.dump(data.test, 4, 'agenda');
+    var tbl_data = [
+        {title:'Row 1'},
+        {title:'Row 2'},
+        {title:'Row 3'}
+    ];
+
+    $.table.setData(tbl_data);
+    Alloy.Globals.CB.Debug.echo('===agenda loadig====', 13, 'agenda.js');
 };
 
 $.back.addEventListener('click', function(e) {
@@ -10,5 +18,13 @@ $.back.addEventListener('click', function(e) {
         controller: 'home',
         animation: Alloy.Globals.CB.UI.AnimationStyle.NavRight,
         data: []
+    });
+});
+
+$.table.addEventListener('click', function(ev) {
+    Alloy.Globals.CB.pushController({
+        controller: 'post',
+        static: true,
+        animation: Alloy.Globals.CB.UI.AnimationStyle.NavLeft
     });
 });
