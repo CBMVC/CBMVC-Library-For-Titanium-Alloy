406d888b647e23564255b95ba312933c62c547d1
Winson <winsonet@gmail.com>
2013-12-06 18:19:52 +0800

    no message

--- app/controllers/agenda.js
+++ app/controllers/agenda.js
@@ -1,30 +1,36 @@
 exports.baseController = "base";
 
 $.onLoad = function() {
-    var data = $.getData();
-    Alloy.Globals.CB.Debug.dump(data.test, 4, 'agenda');
-    var tbl_data = [
-        {title:'Row 1'},
-        {title:'Row 2'},
-        {title:'Row 3'}
+	var data = $.getData();
+	Alloy.Globals.CB.Debug.dump(data.test, 4, 'agenda');
+	var tbl_data = [
+        {
+		title: 'Row 1'
+	},
+        {
+		title: 'Row 2'
+	},
+        {
+		title: 'Row 3'
+	}
     ];
 
-    $.table.setData(tbl_data);
-    Alloy.Globals.CB.Debug.echo('===agenda loadig====', 13, 'agenda.js');
+	$.table.setData(tbl_data);
+	Alloy.Globals.CB.Debug.echo('===agenda loadig====', 13, 'agenda.js');
 };
 
 $.back.addEventListener('click', function(e) {
-    Alloy.Globals.CB.pushController({
-        controller: 'home',
-        animation: Alloy.Globals.CB.UI.AnimationStyle.SlideRight,
-        data: []
-    });
+	Alloy.Globals.CB.pushController({
+		controller: 'home',
+		animation: Alloy.Globals.CB.UI.AnimationStyle.SlideRight,
+		data: []
+	});
 });
 
 $.table.addEventListener('click', function(ev) {
-    Alloy.Globals.CB.pushController({
-        controller: 'post',
-        static: true,
-        animation: Alloy.Globals.CB.UI.AnimationStyle.NavLeft
-    });
+	Alloy.Globals.CB.pushController({
+		controller: 'post',
+		static: true,
+		animation: Alloy.Globals.CB.UI.AnimationStyle.NavLeft
+	});
 });
